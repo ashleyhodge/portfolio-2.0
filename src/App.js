67bf814201b069351cd72e2d54212ca9f8711fc2
ashleyhodge <ashhodge@comcast.net>
2022-05-04 22:41:10 -0400

    add footer and links to accounts

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import './App.css';
 import Nav from './components/Nav';
 import Header from './components/Header';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Footer from './components/Footer';
 
 function App() {
   return (
     <div>
       <Header></Header>
       <Nav></Nav>
+      <Footer></Footer>
     </div>
   );
 }
---
+++ src/components/Footer/index.js
@@ -0,0 +1,38 @@
+import React from "react";
+import { ThreeDotsVertical } from "react-bootstrap-icons";
+
+function Footer() {
+  return(
+    <footer className="footer py-4">
+      <div className="container">
+        <div className="row text-center">
+          <div>Copyright &copy; Ashley Hodge 2022</div>
+          <div>
+            <a className="btn btn-social mx-2" href="https://github.com/ashleyhodge" aria-label="Twitter">
+              <div>
+                <i class="fab fa-github change"></i>
+              </div>
+            </a>
+            <a className="btn btn-social mx-2" href="https://twitter.com/Ashley__Hodge" aria-label="Twitter">
+              <div>
+                <i class="fa-brands fa-twitter change"></i>
+              </div>
+            </a>
+            <a className="btn btn-social mx-2" href="https://www.facebook.com/ashley.hodge.7165" aria-label="Facebook">
+              <div>
+                <i class="fab fa-facebook-f change"></i>
+              </div>
+            </a>
+            <a className="btn btn-social mx-2" href="https://www.linkedin.com/in/ashley-hodge-227285138/" aria-label="LinkedIn">
+              <div>
+                <i class="fab fa-linkedin-in change"></i>
+              </div>
+            </a>
+          </div>
+        </div>
+      </div>
+    </footer>
+  )
+}
+
+export default Footer;
