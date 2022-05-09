66281b13478afde266c4b971a1af434c487592bf
ashleyhodge <ashhodge@comcast.net>
2022-05-09 13:35:23 -0400

    add about me setup

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Header from './components/Header';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Footer from './components/Footer';
 import Portfolio from './components/Portfolio';
+import About from './components/About';
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
       <Header></Header>
       <Nav></Nav>
       <Portfolio></Portfolio>
+      <About></About>
       <Footer></Footer>
     </div>
   );
---
+++ src/components/About/index.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+
+function About() {
+  return (
+    <section>
+      <div className="container">
+        <div className="text-center">
+          <h1 id="about-me">About</h1>
+          <h4>A little about who I am and how I got here!</h4>
+        </div>
+      </div>
+    </section>
+  )
+}
+
+export default About;
