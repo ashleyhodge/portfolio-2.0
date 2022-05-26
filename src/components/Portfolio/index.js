85f77bb70c42ad5c7bd62ca2ebe42fa360813176
ashleyhodge <ashhodge@comcast.net>
2022-05-25 23:20:02 -0400

    random

--- src/components/Portfolio/index.js
+++ src/components/Portfolio/index.js
@@ -11,6 +11,9 @@ function Portfolio() {
             <h1 className="title">Portfolio</h1>
             <h4 className="description">This consists of projects that I have completed, as well as projects that I will continue to work on!</h4>
           </div>
+
+
+          
         </div>
         <Projects ></Projects>
       </div>
